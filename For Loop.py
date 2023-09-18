eaf66c198c37684e48d466f2ddf7ac064c1c89da
NancyR17 <111511876+NancyR17@users.noreply.github.com>
2023-09-18 15:37:34 +0530

    For Loop

---
+++ For Loop.py
@@ -0,0 +1,21 @@
+name = "Nancy"
+for n in name:
+  print(n)
+  if (n == "c"):
+    print("This is a special character")
+
+names = ["Nancy", "Dhavish", "Gulshan"]
+for person in names:
+  print(person)
+
+for k in range(5):
+  print(k)
+  print(k + 1)
+
+for m in range(0, 100):
+  print(m+1)
+
+for k in range(0,12, 3):
+  print(k+1)
+for k in range(1, 12, 3):
+  print(k) 
