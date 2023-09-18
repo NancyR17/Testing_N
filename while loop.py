9a83b0929667512b40b831bff62c2c0127a5b89a
NancyR17 <111511876+NancyR17@users.noreply.github.com>
2023-09-18 18:26:26 +0530

    while loop

---
+++ while loop.py
@@ -0,0 +1,32 @@
+count = 5
+while (count >0):
+   print(count)
+   count = count -1
+
+i = int(input("Enter the number: "))
+while(i<=9):
+  i = int(input("Enter the number: "))
+  print(i)
+
+print("Done with while loop")
+
+x = -5
+while(x > 0):
+  print(x)
+  x = x + 1
+else:
+  print("counter is 0")
+
+count = 5
+print(count)
+while (count < 0):
+  print(count)
+  count = count -1
+else:
+print ("This is do while loop")
+
+while True:
+  number = int(input("Enter a positive number: "))
+  print(number)
+  if not number > 0:
+    break
